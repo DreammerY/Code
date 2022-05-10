cfd8c241460e565684915562d5b5b62e260bb293
yangbaorui <v_baoryang@tencent.com>
2022-05-10 14:30:41 +0800

    all

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="sb doubao Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+/* #app{
+  height: 100%;
+  width: 100%;
+} */
+</style>
