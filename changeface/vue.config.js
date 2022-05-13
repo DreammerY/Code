04bb1aefad1a6f7401f93192d73a159fd7572362
yangbaorui <v_baoryang@tencent.com>
2022-05-13 17:11:12 +0800

    补充

--- changeface/vue.config.js
+++ changeface/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
         proxy: {
             '/test': {
                 // 此处的写法，目的是为了 将 /api 替换成 https://www.baidu.com/
-                target: 'http://47.97.25.111:3308/',  // target为需要访问的地址
+                target: 'http://47.97.25.111:8088/',  // target为需要访问的地址
                 // 允许跨域
                 changeOrigin: true,
                 ws: true,
