57a79b7c7fbf38f7e2071d4c276655175326480e
yangbaorui <v_baoryang@tencent.com>
2022-05-26 11:14:28 +0800

    tab

--- changeface/src/components/HelloWorld.vue
+++ changeface/src/components/HelloWorld.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="hello">
     <div class="left" ref="left">
-      <!-- <div class="left_top">图像处理</div> -->
-      <router-link to="index" class="left_top">图像处理</router-link>
+      <div class="left_top" @click="handleIndex">图像处理</div>
+      <!-- <router-link to="index" class="left_top" @click="handleIndex">图像处理</router-link> -->
       <div>
       <el-menu 
         :default-active="activeIndex"
         class="el-menu-vertical-demo"
         background-color="#545c64"
         text-color="#fff"
-        :router="true"
+        @select="select"
         active-text-color="#ffd04b">
         <el-submenu index="1">
           <template slot="title">
@@ -55,11 +55,22 @@
     <!-- right -->
     <div class="right">
       <div class="right_top">
-        <el-menu :default-active="activeIndex2" mode="horizontal"  background-color="#545c64"
+        <!-- <el-menu :default-active="activeIndex2" mode="horizontal"  background-color="#545c64"
           text-color="#fff" active-text-color="#ffd04b"  :router="true">
             <el-menu-item index="1" class="right_top_item">处理中心</el-menu-item>
             <el-menu-item index="2">处理中心</el-menu-item>
-        </el-menu>
+        </el-menu> -->
+        <el-tabs v-model="editableTabsValue" type="card" closable @tab-remove="removeTab" @tab-click="tabClick">
+          <el-tab-pane
+            v-for="(item) in editableTabs"
+            :key="item.name"
+            :editable="true"
+            :label="item.title"
+            :name="item.name"
+            :stretch="false"
+          >
+          </el-tab-pane>
+        </el-tabs>
       </div>
       <div class="right_bottom">
         <router-view @changIndex="changIndex"></router-view>
@@ -69,17 +80,89 @@
 </template>
 
 <script>
+var  tabBar = {
+  createface:"人脸生成",
+  collectface:"人脸收藏",
+  changeface:"机器换脸",
+  cutperson:"人物分割",
+  replacebackground:"背景替换",
+  backgroundlist:"背景列表",
+  addbackground:"新增背景",
+}
 export default {
     data() {
       return {
         closeShow: true,
         openShow: false,
-        right_top_items:["首页","收藏"],
         activeIndex: '1',
         activeIndex2: '1',
+        // top tab
+        editableTabsValue: 'index',
+        editableTabs: [{title:"首页",name:'index',path:'index'}],
+        emuns:[],
       };
     },
     methods: {
+      // 选中左侧导航时
+      select(path){
+        if(this.$route.name==path) return
+        this.$router.push(path)
+        this.editableTabsValue = path
+        var exit = false
+        this.editableTabs.forEach(item => {
+          if(item.path == path){
+            exit = true
+          }
+        })
+        if(exit){
+          this.tabClick({name:path})
+          return
+        }
+        this.editableTabs = [...this.editableTabs,{title:tabBar[path],name:path,path:path}]
+      },
+      // 头部tab点击时
+      tabClick(tab){
+        if(this.$route.name==tab.name) return
+        this.activeIndex = tab.name
+        this.$router.push(tab.name)
+      },
+      // 点击首页
+      handleIndex(){
+          if(this.$route.name=='index') return
+          this.$router.push('index')
+          var exit = false
+          this.editableTabs.forEach(item => {
+            if(item.path == "index"){
+              exit = true
+            }
+          })
+          if(exit){
+            this.tabClick({name:"index"})
+            return
+          }
+          this.editableTabs = [...this.editableTabs,{title:"首页",name:"index",path:"index"}]
+          this.editableTabsValue = "index"
+      },
+      removeTab(targetName) {
+        if(this.editableTabs.length == 1) return
+        let tabs = this.editableTabs;
+        let activeName = this.editableTabsValue;
+        if (activeName === targetName) {
+          tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+              let nextTab = tabs[index + 1] || tabs[index - 1];
+              if (nextTab) {
+                activeName = nextTab.name;
+                this.activeIndex = nextTab.name
+                this.$router.push(nextTab.path)
+              }
+            }
+          });
+        }
+        
+        this.editableTabsValue = activeName;
+        this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+      },
       changIndex(index){
         this.activeIndex = index
       },
@@ -89,11 +172,18 @@ export default {
         this.closeShow = false
         this.openShow = true
       },
-       handleOpen(){
+      handleOpen(){
         // this.leftShow = true
         this.$refs.left.style.display = "block"
         this.closeShow = true
         this.openShow = false
+      }
+    },
+    watch:{
+      editableTabsValue(newval){
+        if(newval=="index"){
+          this.activeIndex = ""
+        }
       }
     }
 }
@@ -103,15 +193,20 @@ export default {
 <style scoped>
 .el-menu-vertical-demo:not(.el-menu--collapse) {
     width: 200px;
-    min-height: 400px;
+    min-height: 550px;
 }
 .hello{
   display: flex;
   position: relative;
+  height: 100%;
 }
 .hello .left{
   position: relative;
+  height: 100%;
   width: 200px;
+}
+.left_top:hover{
+  cursor: pointer;
 }
 .hello .left .left_top{
   display: block;
@@ -126,7 +221,10 @@ export default {
   font-style: normal;
   letter-spacing: 0px;
   font-family: SourceHanSansSC;
-  margin-bottom: 20px;
+  margin-bottom: 5px;
+}
+.hello .left .el-menu{
+  height: 100%;
 }
 /* 右侧*/
 .hello .right{
@@ -136,7 +234,7 @@ export default {
 }
 .hello .right .right_top{
   height: 60px;
-  margin-bottom: 20px;
+  margin-bottom: 5px;
 }
 .hello .right .right_top .right_top_item{
   padding-right: 10px;
